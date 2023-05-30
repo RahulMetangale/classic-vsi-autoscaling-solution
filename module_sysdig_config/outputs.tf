d7b9ccd8b770ac1de0742eefa80a865043e40cbf
Rahul Metangale <5917001+RahulMetangale@users.noreply.github.com>
2023-05-30 14:12:53 +0530

    updated variable state to nonsensitive

--- module_sysdig_config/outputs.tf
+++ module_sysdig_config/outputs.tf
@@ -1,3 +1,4 @@
 output "sysdig_dashboard_url" {
     value = "${ibm_resource_key.resource_key.credentials["Sysdig Endpoint"]}/#/dashboards/${sysdig_monitor_dashboard.autoscale_dashboard.id}"
+    sensitive = false
 }
